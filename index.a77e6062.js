8477f0183a1daf646b04e8d6de49babed46eeb92
nadiakhoptiuk <98387901+nadiakhoptiuk@users.noreply.github.com>
2022-09-28 21:51:41 +0000

    Deploying to gh-pages from @ nadiakhoptiuk/finance-ledger@7b85281d4d2a8b7d403995939849e010222ad41b 🚀

--- index.a77e6062.js
+++
@@ -1,2 +0,0 @@
-const s=document.querySelector("#js-header");window.addEventListener("scroll",(function(){let e=window.innerWidth<768?"mobile":"desktop";"mobile"===e&&(window.scrollY>120?s.classList.add("visible"):s.classList.remove("visible")),"desktop"===e&&(window.scrollY>70?s.classList.add("visible"):s.classList.remove("visible"))}));
-//# sourceMappingURL=index.a77e6062.js.map
--- index.eea59dbc.js
+++
@@ -1,2 +0,0 @@
-!function(){var i=document.querySelector("#js-header");window.addEventListener("scroll",(function(){var s=window.innerWidth<768?"mobile":"desktop";"mobile"===s&&(window.scrollY>120?i.classList.add("visible"):i.classList.remove("visible")),"desktop"===s&&(window.scrollY>70?i.classList.add("visible"):i.classList.remove("visible"))}))}();
-//# sourceMappingURL=index.eea59dbc.js.map
