d159c1939973e482610d5fa70af9b6834bf0a09e
Nadia Khoptiuk <holineinadia@gmail.com>
2022-09-29 03:00:57 +0300

    feature / add gallery and change it styles

---
+++ src/js/gallery.js
@@ -0,0 +1,13 @@
+let galleryLightbox = new SimpleLightbox('.cases__gallery a', {
+  overlay: true,
+  overlayOpacity: 0.8,
+  scrollZoom: true,
+  maxZoom: 2,
+  doubleTapZoom: 1.5,
+  animationSpeed: 150,
+  animationSlide: true,
+  nav: true,
+  close: true,
+  spinner: true,
+  showCounter: false,
+});
