2bb647deb912ab2cce525c02bbb5599832e327f0
nadiakhoptiuk <98387901+nadiakhoptiuk@users.noreply.github.com>
2022-09-29 00:01:31 +0000

    Deploying to gh-pages from @ nadiakhoptiuk/finance-ledger@d159c1939973e482610d5fa70af9b6834bf0a09e 🚀

---
+++ index.ec734b1d.js
@@ -0,0 +1,2 @@
+new SimpleLightbox(".cases__gallery a",{overlay:!0,overlayOpacity:.8,scrollZoom:!0,maxZoom:2,doubleTapZoom:1.5,animationSpeed:150,animationSlide:!0,nav:!0,close:!0,spinner:!0,showCounter:!1});
+//# sourceMappingURL=index.ec734b1d.js.map
